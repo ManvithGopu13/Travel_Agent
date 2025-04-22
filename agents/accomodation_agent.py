f1f3076b60d34593e6891f49391d9f2d0ffc09ab
ManvithGopu13 <manvithgopu1394@gmail.com>
2025-04-22 19:11:35 +0530

    Implemented Accomodation searching agent

---
+++ agents/accomodation_agent.py
@@ -0,0 +1,15 @@
+from langchain_ollama import ChatOllama
+from duckduckgo_search import DDGS
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.schema.runnable import RunnableLambda
+
+search = DuckDuckGoSearchRun()
+llm = ChatOllama(model = "qwen2.5:7b")
+
+async def _accomodation(location: str) -> str:
+    query = f"Best hostels , dormitories , public restplaces to stay in {location} with pricing and website links."
+    results = search.run(query)
+    prompt = f"List good hostels/dorms/dharamasatras or public restplaces in {location} from this {results}\n\n Provide name, type, price per night , location and contact."
+    return llm.invoke(prompt)
+
+accomodation_agent = RunnableLambda(func=_accomodation)
